640712f25fa5d64c2d65fa6ed12ae1be5b2d8a3f
Leo Blanco <lbmajaw@gmail.com>
2023-09-04 17:30:12 +0100

    Adjustments in the environment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL : "https://localhost:44370",
+    baseURL : "https://localhost:7083",
+    // baseURL : "https://localhost:44370",
 })
 
 export default api;

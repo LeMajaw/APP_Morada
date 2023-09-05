eb1f395e5d97ef28eb7b18cfa8d6f2a55ff0b533
Leo Blanco <lbmajaw@gmail.com>
2023-09-05 15:54:18 +0100

    Visual adjustments

--- src/pages/NovaMorada/index.js
+++ src/pages/NovaMorada/index.js
@@ -1,48 +1,50 @@
-import React , {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.css';
-import {Link, useHistory, useParams} from 'react-router-dom';
-import {FiCornerDownLeft, FiUserPlus } from 'react-icons/fi';
+import { Link, useHistory, useParams } from 'react-router-dom';
+import { FiCornerDownLeft } from 'react-icons/fi';
+import { GrReturn } from 'react-icons/gr';
+import { BsHouseAdd } from 'react-icons/bs';
 import api from '../../services/api';
 
-export default function NovaMorada(){
-   const [id,setId]= useState(null);
-  
+export default function NovaMorada() {
+   const [id, setId] = useState(null);
+
    const [morada, setMorada] = useState('');
-   
+
    const [codigoPostal, setCodigoPostal] = useState('');
-   
+
    const [rua, setRua] = useState('');
-   
+
    const [freguesia, setFreguesia] = useState('');
-   
+
    const [concelho, setConcelho] = useState('');
-   
+
    const [distrito, setDistrito] = useState('');
-   
+
    const [pais, setPais] = useState('');
 
-   const {moradaId} = useParams();
-   
+   const { moradaId } = useParams();
+
    const history = useHistory();
 
    const token = localStorage.getItem('token');
-   
-   const authorization = {
-      headers : {
-         Authorization : `Bearer ${token}`
-      }
-   }  
 
-   useEffect(()=>{
-      if(moradaId === '0')
-      return;
+   const authorization = {
+      headers: {
+         Authorization: `Bearer ${token}`
+      }
+   }
+
+   useEffect(() => {
+      if (moradaId === '0')
+         return;
       else
-      loadMorada();  
+         loadMorada();
    }, moradaId)
 
-   async function loadMorada(){
-      try{
-         const response = await api.get(`api/moradas/${moradaId}`,authorization) ;
+   async function loadMorada() {
+      try {
+         const response = await api.get(`api/morada/${moradaId}`, authorization);
 
          setId(response.data.id);
          setMorada(response.data.morada);
@@ -52,9 +54,9 @@ export default function NovaMorada(){
          setConcelho(response.data.concelho);
          setDistrito(response.data.distrito);
          setPais(response.data.pais);
-         }catch(error){
-            alert('Erro ao recuperar a morada ' + error);
-            history.push('/moradas');
+      } catch (error) {
+         alert('Erro ao recuperar a morada ' + error);
+         history.push('/moradas');
       }
    }
 
@@ -71,63 +73,66 @@ export default function NovaMorada(){
          pais,
       }
 
-      try{
-         if(moradaId==='0')
-         {
-            await api.post('api/moradas',data,authorization);
+      try {
+         if (moradaId === '0') {
+            await api.post('api/morada', data, authorization);
          }
-         else
-         {
-            data.id= id;
-            await api.put(`api/moradas/${id}`,data,authorization)
+         else {
+            data.id = id;
+            await api.put(`api/morada/${id}`, data, authorization)
          }
-      }catch(error){
+      } catch (error) {
          alert('Erro ao gravar morada ' + error);
       }
       history.push('/moradas');
    }
 
-   return(
+   return (
       <div className="nova-morada-container">
          <div className="content">
-         <section className="form"><FiUserPlus size="105" color="#17202a"/>
-            <h1>{moradaId === '0'? 'Incluir Nova Morada' : 'Atualizar Morada'}</h1>
-            <Link className="back-link" to="/moradas">
-               <FiCornerDownLeft size="25" color="#17202a"/> Retornar
-            </Link>
-         </section>
-         
-         <form onSubmit={saveOrUpdate}>
-            <input  placeholder="Morada" 
-               value={morada}
-               onChange= {e=> setMorada(e.target.value)}
-            />
-            <input  placeholder="Código Postal" 
-               value={codigoPostal}
-               onChange={e => setCodigoPostal(e.target.value)}
-            />
-            <input  placeholder="Rua" 
-               value={rua}
-               onChange={e => setRua(e.target.value)}
-            />
-               <input  placeholder="Freguesia" 
-               value={freguesia}
-               onChange={e => setFreguesia(e.target.value)}
-            />
-               <input  placeholder="Concelho" 
-               value={concelho}
-               onChange={e => setConcelho(e.target.value)}
-            />
-               <input  placeholder="Distrito" 
-               value={distrito}
-               onChange={e => setDistrito(e.target.value)}
-            />
-               <input  placeholder="País" 
-               value={pais}
-               onChange={e => setPais(e.target.value)}
-            />
-               <button className="button" type="submit">{moradaId === '0'? 'Incluir ' : 'Atualizar '}</button>
-         </form>
+            <section className="form"><BsHouseAdd size="300" color="#17202a" />
+               <h1>{moradaId === '0' ? 'Incluir Nova Morada' : 'Atualizar Morada'}</h1>
+               <Link className="back-link" to="/moradas">
+                  {/* <GiReturnArrow size="25" color="#17202a"/> */}
+                  {/* <FiCornerDownLeft size="25" color="#17202a"/> Retornar GiReturnArrow */}
+
+                  <div className="button-retorno">
+                     <GrReturn size={50} style={{marginTop: 10}} />
+                  </div>
+               </Link>
+            </section>
+
+            <form onSubmit={saveOrUpdate}>
+               <input placeholder="Morada"
+                  value={morada}
+                  onChange={e => setMorada(e.target.value)}
+               />
+               <input placeholder="Código Postal"
+                  value={codigoPostal}
+                  onChange={e => setCodigoPostal(e.target.value)}
+               />
+               <input placeholder="Rua"
+                  value={rua}
+                  onChange={e => setRua(e.target.value)}
+               />
+               <input placeholder="Freguesia"
+                  value={freguesia}
+                  onChange={e => setFreguesia(e.target.value)}
+               />
+               <input placeholder="Concelho"
+                  value={concelho}
+                  onChange={e => setConcelho(e.target.value)}
+               />
+               <input placeholder="Distrito"
+                  value={distrito}
+                  onChange={e => setDistrito(e.target.value)}
+               />
+               <input placeholder="País"
+                  value={pais}
+                  onChange={e => setPais(e.target.value)}
+               />
+               <button className="button" type="submit">{moradaId === '0' ? 'Incluir ' : 'Atualizar '}</button>
+            </form>
 
          </div>
       </div>
